db3b0922e32172e2d0a1ae70b12ece775843f9a6
Polpedu <emailparaoedu@gmail.com>
2022-05-02 19:08:56 +0100

    check stor tambem

--- 2015_6.c
+++ 2015_6.c
@@ -1,7 +1,11 @@
 #define PIPE_NAME1 "PIPE1"
-#define PIPE_NAME2 “PIPE2”
+#define PIPE_NAME2 "PIPE2"
 
 #include <stdio.h>
+#include <pthread.h>
+#include <sys/unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 
 // variáveis globais
 int fd_named_pipe[2];
@@ -9,24 +13,53 @@ int soma_total=0;
 
 
 // Outras funções
-
-
-
+//De 5 em 5 segundos uma thread responsável pelas estatísticas irá escrever no ecrã o número de leituras realizadas e o 
+//valor da variável soma_total.
 
 
 
 
 
 // Função a ser executada pelas thread que lê dos named pipes
-
-
+/*A thread que lê dados dos named pipes termina quando tiver lido o valor -1 de qualquer um dos named piped. Depois 
+disso, a thread que apresenta as estatísticas apresentará uma última vez as estatísticas e também sairá. Por fim, todos os 
+recursos em utilização devem ser destruídos (incluindo o named pipe).*/
+void *thread_read_named_pipe(void *arg)
+{
+    //read from named pipe
+    int read_value;
+    while(1)
+    {
+        read(fd_named_pipe[0], &read_value, sizeof(int));
+        if(read_value == -1)
+        {
+            break;
+        }
+        soma_total += read_value;
+    }
+}
 
 
 
 
 // Função a ser executada pelas thread que lê e escreve a estatística no ecra
+// De 5 em 5 segundos uma thread responsável pelas estatísticas irá escrever no ecrã o número de leituras realizadas e o 
+//valor da variável soma_total.
+void *thread_write_statistics(void *arg)
+{
+    int n_leituras=0;
+    
+    while(1)
+    {
+        // escrever no ecrã
+        printf("[%d] %d. Soma total: %d\n", getpid(),n_leituras, soma_total);
+        
+        // esperar 5 segundos
+        sleep(5);
 
-
+        n_leituras++;
+    }
+}
 
 
 /*
@@ -47,6 +80,9 @@ recursos em utilização devem ser destruídos (incluindo o named pipe).
 bom funcionamento do programa.
     • Não deve existir espera activa
 */
+
+    // gcc –lpthread –D_REENTRANT –Wall fich.c –o fich
+
 int main(void) {
     // criar pipes e terão o nome “PIPE1” e “PIPE2”: if they were unnamed: pipe(channel)
     /*
@@ -54,25 +90,41 @@ int main(void) {
 cada 5 segundos. A estatística é constituída pelo número de inteiros lidos e pela sua soma total. Deverá ainda ter em conta as 
 seguintes funcionalidades e restrições:
 */
-// Creates the named pipe if it doesn't exist yet
+
+    // Creates the named pipe if it doesn't exist yet
     if ((mkfifo(PIPE_NAME1, O_CREAT|O_EXCL|0600)<0) && (errno!= EEXIST)) {
         perror("Cannot create pipe %s", PIPE_NAME1);
         exit(0);
     }
     if ((mkfifo(PIPE_NAME2, O_CREAT|O_EXCL|0600)<0) && (errno!= EEXIST)) {
-        perror("Cannot create pipe %s", PIPE_NAME2);
+        perror("Cannot create pipe %s", PIPE_NAME2); //#include <errno.h>
+        exit(0);
+    }
+    
+    // Creates the named pipe if it doesn't exist yet
+    if ((fd_named_pipe[0] = open(PIPE_NAME1, O_RDONLY)) < 0) { //open pipes
+        perror("Cannot open pipe %s", PIPE_NAME1);
+        exit(0);
+    }
+    if ((fd_named_pipe[1] = open(PIPE_NAME2, O_WRONLY)) < 0) { //open pipes
+        perror("Cannot open pipe %s", PIPE_NAME2);
         exit(0);
     }
 
-
-
-
-
-    // criar threads
-    pthread_t thread_leitura;
+    // Creates the thread that reads from the named pipe
     pthread_t thread_estatistica;
+    pthread_create(&thread_estatistica, NULL, thread_write_statistics, NULL);
+
+    // Creates the thread that writes on the screen
+    pthread_t thread_leitura;
+    pthread_create(&thread_leitura, NULL, thread_read_named_pipe, NULL);
 
 
 
     // Esperar pelo fim das threads e remover recursos
+    pthread_join(thread_leitura, NULL); // wait for termination of thread_leitura
+    pthread_join(thread_estatistica, NULL); // wait for termination of thread_estatistica
+    unlink(PIPE_NAME1); //#include <sys/unistd.h>
+    unlink(PIPE_NAME2);
+    return 0;
 }
